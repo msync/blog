ffc8880b1484dea2c33bd8fba801d96fed6adebe
Ravindra Jaju <jaju@msync.org>
2024-03-12 13:14:19 +0530

    New updates - style, some content.

--- notes/deep-learning-with-python/test/org/msync/dl_python_test.clj
+++
@@ -1,7 +0,0 @@
-(ns org.msync.dl-python-test
-  (:require [clojure.test :refer :all]
-            [org.msync.dl-python :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
