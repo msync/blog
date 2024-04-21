346257188c9ef36e1bd5963b874461fbbaaa2203
Ravindra Jaju <jaju@msync.org>
2024-04-21 23:04:09 +0530

    Test commit - for attaching python files to the publishing department

---
+++ notes/an-overview-for-llm-experienced-beginners/constants.py
@@ -0,0 +1,9 @@
+# [[file:llm.org::*Code Constants][Code Constants:1]]
+mistral_model_name = "mistralai/Mistral-7B-v0.1"
+mistral_model_embeddings_extract_file = "mistral-embeddings.pth"
+
+llama3_model_name = "meta-llama/Meta-Llama-3-8B"
+llama3_model_embeddings_extract_file = "meta-llama-3-embeddings.pth"
+
+glove_model_name = "glove-wiki-gigaword-100"
+# Code Constants:1 ends here
---
+++ notes/an-overview-for-llm-experienced-beginners/utils.py
@@ -0,0 +1,48 @@
+# [[file:llm.org::*Utils][Utils:1]]
+import torch
+import torch.nn as nn
+from functools import cache
+from transformers import AutoTokenizer, AutoModel
+
+class EmbeddingModel(nn.Module):
+    def __init__(self, vocab_size, embedding_dim):
+        super(EmbeddingModel, self).__init__()
+        self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim)
+
+    def forward(self, input_ids):
+        return self.embedding(input_ids)
+
+
+def print_tokens(tokenizer, input_ids_tensor):
+    token_texts = [tokenizer.decode([token_id], skip_special_tokens=True) for token_id in input_ids_tensor[0]]
+    header = f"{'Token':<10} | {'ID':<8}"
+    print(f"{'='*30}\n{'Tokens and their IDs':^30}\n{'='*30}")
+    print(header)
+    print(f"{'-'*10}-+-{'-'*17}")
+
+    for idx, token_id in enumerate(input_ids_tensor[0]):
+        token_text = token_texts[idx]
+        print(f"{token_text:<10} | {token_id:<20}")
+
+    print(f"{'='*30}")
+
+
+def extract_embeddings(model_name, embeddings_filename):
+    model = AutoModel.from_pretrained(model_name)
+    embeddings = model.get_input_embeddings()
+    print(f"Extracted embeddings layer for {model_name}: {embeddings}")
+    torch.save(embeddings.state_dict(), embeddings_filename)
+
+
+# Optimizing on load times for REPL-workflows - we cache
+@cache
+def load_embeddings(embeddings_filename):
+    embeddings_data = torch.load(embeddings_filename)
+    weights = embeddings_data["weight"]
+    vocab_size, dimensions = weights.size()
+
+    embeddings = EmbeddingModel(vocab_size, dimensions)
+    embeddings.embedding.weight.data = weights
+    embeddings.eval()
+    return embeddings
+# Utils:1 ends here
